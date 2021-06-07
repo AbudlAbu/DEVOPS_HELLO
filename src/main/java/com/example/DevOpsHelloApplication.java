6cb7c5f3665036679b0b0ea40281703b01966f5e
Abu I <84893883+AbudlAbu@users.noreply.github.com>
2021-06-07 11:19:04 +0530

    First commit

---
+++ src/main/java/com/example/DevOpsHelloApplication.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsHelloApplication.class, args);
+		System.out.println("Hello_devOps");
+	}
+
+}
